3a9ebfd20b5ecfae3f8e87767b68b0b55260ca32
Mcleish <40162362@live.napier.ac.uk>
2016-09-26 11:39:38 +0100

    Observer pattern demo

--- src/Observer/ObserverDemo.java
+++ src/Observer/ObserverDemo.java
@@ -4,11 +4,27 @@ public class ObserverDemo {
 	
 	public static void main (String[] args){
 		
-		PopStar Jason = new PopStar();
+		PopStar Jason  = new PopStar();
+		PopStar Queen = new PopStar();
 		Fans f1 = new Fans("Steven",Jason);
 		Fans f2 = new Fans("Mike",Jason);
+		Fans f3 = new Fans("Jess",Queen);
+		Fans f4 = new Fans("Emily",Queen);
 		
+		Jason.registerObserver(f4);
 		Jason.freshNews("New album out");
+		
+		Jason.removeObserver(f1);
+		
+		
+		
+		Jason.freshNews("New Alubum Cancelled");
+		
+		Queen.freshNews("Going on tour");
+		
+		
+		
+		
 		
 		
 		
