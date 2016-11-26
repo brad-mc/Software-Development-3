7c7a7518df342f55bb46fde0c9d39410f13b5d77
Brad Mc <Brad Mc>
2016-11-26 13:02:19 +0000

    Added labels to create 4x4 grid

--- SkyWars/src/Home.java
+++ SkyWars/src/Home.java
@@ -23,7 +23,7 @@ import javax.swing.JButton;
 public class Home extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
-	private JLabel label;
+	private JLabel label1,label2,label3,label4,label5,label6,label7,label8,label9,label10,label11,label12,label13,label14,label15,label16;
 	ShipFactory theFactory;
 	
 
@@ -69,14 +69,61 @@ public class Home extends JFrame implements ActionListener {
 		contentPane.add(panel);
 		
 		
-		
+		Border border = LineBorder.createGrayLineBorder();
 		JButton btn = new JButton("Change");
         btn.setActionCommand("myButton");
         btn.addActionListener(this);
-        label = new JLabel("flag");
-        
+        label1 = new JLabel("1");
+        label2 = new JLabel("2");
+        label3 = new JLabel("3");
+        label4 = new JLabel("4");
+        label5 = new JLabel("5");
+        label6 = new JLabel("6");
+        label7 = new JLabel("7");
+        label8 = new JLabel("8");
+        label9 = new JLabel("9");
+        label10 = new JLabel("10");
+        label11 = new JLabel("11");
+        label12 = new JLabel("12");
+        label13 = new JLabel("13");
+        label14 = new JLabel("14");
+        label15 = new JLabel("15");
+        label16 = new JLabel("16");
+       
         panel.add(btn);
-        panel_1.add(label);
+        panel_1.add(label1);
+        panel_1.add(label2);
+        panel_1.add(label3);
+        panel_1.add(label4);
+        panel_1.add(label5);
+        panel_1.add(label6);
+        panel_1.add(label7);
+        panel_1.add(label8);
+        panel_1.add(label9);
+        panel_1.add(label10);
+        panel_1.add(label11);
+        panel_1.add(label12);
+        panel_1.add(label13);
+        panel_1.add(label14);
+        panel_1.add(label15);
+        panel_1.add(label16);
+        
+        label1.setBorder(border);
+        label2.setBorder(border);
+        label3.setBorder(border);
+        label4.setBorder(border);
+        label5.setBorder(border);
+        label6.setBorder(border);
+        label7.setBorder(border);
+        label8.setBorder(border);
+        label9.setBorder(border);
+        label10.setBorder(border);
+        label11.setBorder(border);
+        label12.setBorder(border);
+        label13.setBorder(border);
+        label14.setBorder(border);
+        label15.setBorder(border);
+        label16.setBorder(border);
         
 		
 		
@@ -177,7 +224,7 @@ public class Home extends JFrame implements ActionListener {
         	
         	Ship ship;
 			ship = this.theFactory.createShip(n);
-            label.setText(ship.getType());
+            label1.setText(ship.getType());
         }
     }
 	
