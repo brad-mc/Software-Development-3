3a440fe72973f14458c4c1ef069c69d56a4f90f5
brad-mc <bradley_4@live.co.uk>
2016-11-26 21:27:37 +0000

    '

--- SkyWars/src/Game.java
+++ SkyWars/src/Game.java
@@ -1,9 +1,60 @@
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Game {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static ArrayList<Point> moves = new ArrayList<Point>();
+	public static int  x = 0;
+	public static int y = 0;
+	public static void main (String[] args){
+		Home h = new Home();
+		//h.squares[0][0].setText("Hey");
+		//JOptionPane.showMessageDialog(null, "Hi", "InfoBox: " + "test", JOptionPane.INFORMATION_MESSAGE);
+	   // h.squares[0][0].setText("Bye");
+		Random rand = new Random();
+		
+		
+		boolean same = true;
+		while(same ==true){
+			x = rand.nextInt(4);
+			y = rand.nextInt(4);
+			
+			if(x ==0 & y ==0){
+				same = true;
+			}
+			else{
+				same = false;
+				break;
+			}
+		}
+		
+		MasterShip ms = new MasterShip();
+		h.squares[x][y].setText(ms.getType());
+		
+		
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask(){
+			@Override
+		    public void run() {
+				moves = NeighbourNodes.PossibleMoves(x, y);
+				int move;
+				move = rand.nextInt(moves.size());
+				h.squares[x][y].setText("");
+				x = ((int)moves.get(move).getX());
+				y = ((int)moves.get(move).getY());
+				h.squares[x][y].setText(ms.getType());
+			}
+		},1000,500);
+		moves = NeighbourNodes.PossibleMoves(x, y);
+		/*
+		int move;
+		move = rand.nextInt(moves.size() -1);
+		x = ((int)moves.get(move).getX());
+		y = ((int)moves.get(move).getY());
+		h.squares[x][y].setText(ms.getType());
+		*/
 	}
 
 }
--- SkyWars/src/Home.java
+++ SkyWars/src/Home.java
@@ -21,7 +21,7 @@ public class Home extends JFrame {
 	public static int y = 0;
 	
 	public static final int gridSize = 4;
-	private JButton[][] squares = new JButton[gridSize][gridSize];
+	public JButton[][] squares = new JButton[gridSize][gridSize];
 	public Home(){
 		Container cp = getContentPane();
 		cp.setLayout(new GridLayout(gridSize,gridSize));
@@ -42,54 +42,7 @@ public class Home extends JFrame {
 	      setBounds(100, 100, 450, 300);
 	}
 	
-	public static void main (String[] args){
-		Home h = new Home();
-		//h.squares[0][0].setText("Hey");
-		//JOptionPane.showMessageDialog(null, "Hi", "InfoBox: " + "test", JOptionPane.INFORMATION_MESSAGE);
-	   // h.squares[0][0].setText("Bye");
-		Random rand = new Random();
-		
-		
-		boolean same = true;
-		while(same ==true){
-			x = rand.nextInt(4);
-			y = rand.nextInt(4);
-			
-			if(x ==0 & y ==0){
-				same = true;
-			}
-			else{
-				same = false;
-				break;
-			}
-		}
-		
-		MasterShip ms = new MasterShip();
-		h.squares[x][y].setText(ms.getType());
-		
-		
-		Timer timer = new Timer();
-		timer.scheduleAtFixedRate(new TimerTask(){
-			@Override
-		    public void run() {
-				moves = NeighbourNodes.PossibleMoves(x, y);
-				int move;
-				move = rand.nextInt(moves.size());
-				h.squares[x][y].setText("");
-				x = ((int)moves.get(move).getX());
-				y = ((int)moves.get(move).getY());
-				h.squares[x][y].setText(ms.getType());
-			}
-		},1000,500);
-		moves = NeighbourNodes.PossibleMoves(x, y);
-		/*
-		int move;
-		move = rand.nextInt(moves.size() -1);
-		x = ((int)moves.get(move).getX());
-		y = ((int)moves.get(move).getY());
-		h.squares[x][y].setText(ms.getType());
-		*/
-	}
+
 
 
 	public void actionPerformed(ActionEvent e)
